1651048131c5206d8d3f67365d075859eedcaf0b
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-02-07 23:51:31 -0600

    feat: adding react route

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Layout = ({children}) => {
+    return (
+        <div className="Layout">
+            {children}
+        </div>
+    );
+}
+
+export default Layout;
+
