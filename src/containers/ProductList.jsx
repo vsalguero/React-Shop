7a9b0703e8750cc4fd883d0fadec2976452348b1
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-02-10 00:20:45 -0600

    feat: using axios for fetch data from an API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import ProductItem from '../components/ProductItem';
-import '../styles/ProductList.scss';
+import React, { useState, useEffect } from 'react';
+import ProductItem from '@components/ProductItem';
+import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+
+	const [products, setProducts] = useState([]);
+
+	useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response.data);
+
+	}, []);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem />
+				))}
+
 			</div>
 		</section>
 	);
