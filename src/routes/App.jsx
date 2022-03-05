b1bd56c76c4c86fea498c45bb174058c67e50752
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-03-05 00:12:37 -0600

    feat: add to cart button

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -12,32 +12,34 @@ import CreateAccount from '../pages/CreateAccount';
 import Checkout from '../pages/Checkout';
 import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
+import AppContext from '../context/appcontex';
+import useInitialState from '../hooks/useInitialState';
 import '../styles/global.css';
 
 
-
-
-
 const App = () => {
+    const initialState = useInitialState();
     return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
 
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="/login" element={<Login />} />
-                    <Route exact path="/recovery-password" element={<PasswordRecovery />} />
-                    <Route path='*' element={<NotFound />} />
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/login" element={<Login />} />
+                        <Route exact path="/recovery-password" element={<PasswordRecovery />} />
+                        <Route path='*' element={<NotFound />} />
 
-                    <Route exact path="/send-email" element={<SendEmail />} />
-                    <Route exact path="/new-password" element={<NewPassword />} />
-                    <Route exact path="/account" element={<MyAccount />} />
-                    <Route exact path="/signup" element={<CreateAccount />} />
-                    <Route exact path="/checkout" element={<Checkout />} />
-                    <Route exact path="/orders" element={<Orders />} />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+                        <Route exact path="/send-email" element={<SendEmail />} />
+                        <Route exact path="/new-password" element={<NewPassword />} />
+                        <Route exact path="/account" element={<MyAccount />} />
+                        <Route exact path="/signup" element={<CreateAccount />} />
+                        <Route exact path="/checkout" element={<Checkout />} />
+                        <Route exact path="/orders" element={<Orders />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
     );
 }
 
